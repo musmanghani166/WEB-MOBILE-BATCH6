b925f2a7af9be68f0bfc44503c46d1cfb93f0ff4
usman ghani <musmanghani166@gmail.com>
2023-02-16 07:06:11 +0500

    javascript assignment 3

--- Javascript/Assignments Projects/Assignment project 3/script.js
+++ Javascript/Assignments Projects/Assignment project 3/script.js
@@ -13,6 +13,12 @@ document.getElementById("clearoutput").onclick = function () {
     document.getElementById("output").innerHTML = ""
 }
 
+// variable all cities
+let cities = [ 'Faisalabad', 'Lahore', 'Karachi',  'Islamabad', 'Burewala', 'Sheikhupura',  'Kashmir' ]
+ 
+
+
+
 // simple alert
 function simpleAlert() {
     alert("I 'm an alert")
@@ -26,4 +32,43 @@ function printMyName() {
     return;
 }
 document.getElementById('output').innerHTML = myName
+}
+
+// print all cities
+function printAllCities(){
+    document.getElementById('output').innerHTML = " ";
+
+
+ for (let i = 0; i < cities.length; i++) {
+
+let num = i + 1;
+document.getElementById('output').innerHTML += num + ') ' + cities[i] + '<br/>';
+
+ }
+
+}
+
+// add city into the list
+function addCity() {
+    let city = document.getElementById('inputText').value;
+    if (city === "") { 
+    alert("Please type your city name")
+    return;
+}
+cities.push(city);
+document.getElementById('output').innerHTML = '<span style="color: green; font-size: 20px;">' + city + '</span> has been successfully added into list .';
+}
+
+// generatertable
+
+function generateTable() {
+    let number = document.getElementById('inputText').value;
+    if(number === ""){
+        toastifyError('Please enter a number.');
+        return;
+    }
+    let limit = +prompt('Give a number')
+    document.getElementById('output').innerHTML = limit * 7;
+
+
 }
