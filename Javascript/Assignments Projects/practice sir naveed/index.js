b3f0518006d6bc1f626d2dc6663a01d96dc8888e
usman ghani <musmanghani166@gmail.com>
2023-03-26 16:41:14 +0500

    practice sir naved

--- Javascript/Assignments Projects/practice sir naveed/index.js
+++ Javascript/Assignments Projects/practice sir naveed/index.js
@@ -1,8 +1,22 @@
-let statement =+ prompt('Enter your number')
+// let statement =+ prompt('Enter your number')
 
-if (statement % 2 == 0){
-    console.log('you are a number is even')
-}
-else{
-    console.log('you are a number is odd')
-}
+// if (statement % 2 == 0){
+//     console.log('you are a number is even')
+// }
+// else{
+//     console.log('you are a number is odd')
+// }
+var name = "usman"
+// tada type of programming languages
+// " " '' representing language
+    
+// strict programming language  c++, Java, typescript
+
+
+
+//  non strict programming  language - js, python....
+
+//  pascal case - first later always capital for each word
+
+
+//  how to ckeak data type in { typeof()}
