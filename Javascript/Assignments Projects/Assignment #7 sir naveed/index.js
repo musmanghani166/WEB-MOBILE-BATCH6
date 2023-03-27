43c197c4b074f92f4e10607a59956d0fdd248ede
usman ghani <musmanghani166@gmail.com>
2023-03-27 23:14:08 +0500

    js Q.No'6

--- Javascript/Assignments Projects/Assignment #7 sir naveed/index.js
+++ Javascript/Assignments Projects/Assignment #7 sir naveed/index.js
@@ -1,64 +1,64 @@
 
-// // Q.No: 1 js program to find maximum between two numbers
+// Q.No: 1 js program to find maximum between two numbers
 
-// var num1 = + prompt('enter your first number');
-// var num2 = + prompt('enter your second number');
+var num1 = + prompt('enter your first number');
+var num2 = + prompt('enter your second number');
 
-// if (num1 >= num2) {
-//     console.log("your first number is large", num1, "and secnd number is small", num2)
-// }
-// else {
-//     console.log("your second number is large", num2, "and first number is small", num1)
-// }
+if (num1 >= num2) {
+    console.log("your first number is large", num1, "and secnd number is small", num2)
+}
+else {
+    console.log("your second number is large", num2, "and first number is small", num1)
+}
 
-// //  _______________________________________________________________________________________________  //
+//  _______________________________________________________________________________________________  //
 
-// // Q.No:2 js program to find maximum between three numbers
+// Q.No:2 js program to find maximum between three numbers
 
-// var age = + prompt('enter your age')
-// if (age < 15) {
-//     console.log('you are a baby')
-// }
-// else if (age <= 35) {
-//     console.log("you are a  young")
-// }
-// else if (age > 35) {
-//     console.log("you are a old man")
-// }
+var age = + prompt('enter your age')
+if (age < 15) {
+    console.log('you are a baby')
+}
+else if (age <= 35) {
+    console.log("you are a  young")
+}
+else if (age > 35) {
+    console.log("you are a old man")
+}
 
-// //  _______________________________________________________________________________________________  //
+//  _______________________________________________________________________________________________  //
 
-// //  Q.No:3 js program to check whether a number is negative, positive or zero
-//     var num =+ prompt("entar your any number")
+//  Q.No:3 js program to check whether a number is negative, positive or zero
+    var num =+ prompt("entar your any number")
 
-//    if(num <= -1){
-//     console.log("your number is nagitive", num)
-//    }
-//    else if(num > 0){
-//     console.log("your number is positive", num)
-//    }
-//    else {
-//     console.log("your number is zero")
-//    }
-// //  _______________________________________________________________________________________________  //
+   if(num <= -1){
+    console.log("your number is nagitive", num)
+   }
+   else if(num > 0){
+    console.log("your number is positive", num)
+   }
+   else {
+    console.log("your number is zero")
+   }
+//  _______________________________________________________________________________________________  //
 
-// // Q.NO:4  js program to check whether a number is divisible by 5 and 11 or not.
-   
-//    var num =+ prompt("entar your any number")
-//    if(num % 5 == 0){
-//     console.log("your number is complete divide on 5")
-//    }
-//    else if(num % 11 == 0){
-//     console.log("your number is complete divide on 11")
-//    }
-//     else{
-//     console.log(" your number on 5 and 11 not complete  divide")
-//    }
+// Q.NO:4  js program to check whether a number is divisible by 5 and 11 or not.
 
-// //  _______________________________________________________________________________________________  //
+   var num =+ prompt("entar your any number")
+   if(num % 5 == 0){
+    console.log("your number is complete divide on 5")
+   }
+   else if(num % 11 == 0){
+    console.log("your number is complete divide on 11")
+   }
+    else{
+    console.log(" your number on 5 and 11 not complete  divide")
+   }
+
+//  _______________________________________________________________________________________________  //
 
 //  Q.NO:5  js program to check whether a number is even or odd
-  
+
     var num =+ prompt("entar your any number")
    if(num % 2 == 0){
     console.log("your number is even")
@@ -66,3 +66,18 @@
    else{
     console.log("your number is odd")
    }
+
+//  _______________________________________________________________________________________________  //
+
+//   Q.NO:6  js program to check whether a year is leap year or not
+
+var year = + prompt("Entar your Year")
+
+if (year % 4 == 0) {
+    console.log("It will be a leap year")
+}
+else {
+    console.log("This will not be a leap year.")
+}
+
+//   _______________________________________________________________________________________________  //
