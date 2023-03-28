8fc2bdb716ce9f150dfb967eb0402f7994cb103a
usman ghani <musmanghani166@gmail.com>
2023-03-28 15:34:59 +0500

    js sir naveed Q.No;6.7

--- Javascript/Assignments Projects/Assignment #7 sir naveed/index.js
+++ Javascript/Assignments Projects/Assignment #7 sir naveed/index.js
@@ -15,7 +15,7 @@ else {
 
 // Q.No:2 js program to find maximum between three numbers
 
-var age = + prompt('enter your age')
+var age = + prompt('enter your age');
 if (age < 15) {
     console.log('you are a baby')
 }
@@ -29,7 +29,7 @@ else if (age > 35) {
 //  _______________________________________________________________________________________________  //
 
 //  Q.No:3 js program to check whether a number is negative, positive or zero
-    var num =+ prompt("entar your any number")
+    var num =+ prompt("entar your any number");
 
    if(num <= -1){
     console.log("your number is nagitive", num)
@@ -44,7 +44,7 @@ else if (age > 35) {
 
 // Q.NO:4  js program to check whether a number is divisible by 5 and 11 or not.
 
-   var num =+ prompt("entar your any number")
+   var num =+ prompt("entar your any number");
    if(num % 5 == 0){
     console.log("your number is complete divide on 5")
    }
@@ -59,7 +59,7 @@ else if (age > 35) {
 
 //  Q.NO:5  js program to check whether a number is even or odd
 
-    var num =+ prompt("entar your any number")
+    var num =+ prompt("entar your any number");
    if(num % 2 == 0){
     console.log("your number is even")
    }
@@ -71,7 +71,7 @@ else if (age > 35) {
 
 //   Q.NO:6  js program to check whether a year is leap year or not
 
-var year = + prompt("Entar your Year")
+var year = + prompt("Entar your Year");
 
 if (year % 4 == 0) {
     console.log("It will be a leap year")
@@ -81,3 +81,33 @@ else {
 }
 
 //   _______________________________________________________________________________________________  //
+
+//   Q.NO:7  js program to check whether a character is alphabet or not
+
+    var abc = prompt("entar your any alphabet");
+    var alphabat = "qwertyuiopasdfghjklzxcvbnm" ;
+
+    if(abc = alphabat){
+        console.log("Your word is the alphabet")
+    }
+    else{
+        console.log("Your word is  not alphabet")
+    }
+
+//   _______________________________________________________________________________________________  //
+
+// Q.NO:7 js program to input any alphabet and check whether it is vowel or consonant
+
+    var abc = prompt("entar your any alphabet");
+    var vowel = "aeiou" ;
+    var  consonant = "qwrtyplkjhgfdszxcvbnm";
+
+    if(abc = vowel){
+        console.log("Your word is the vowel")
+    }
+    else {
+        console.log("Your word is  consonant")
+    }
+    
+
+    //   _______________________________________________________________________________________________  //
