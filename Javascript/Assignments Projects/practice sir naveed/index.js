9228078d5a4aba64919a66e60299a203d564c3a2
usman ghani <musmanghani166@gmail.com>
2023-03-31 00:28:12 +0500

    ahmad problem solve js progran

--- Javascript/Assignments Projects/practice sir naveed/index.js
+++ Javascript/Assignments Projects/practice sir naveed/index.js
@@ -1,60 +1,67 @@
-let statement =+ prompt('Enter your number')
+// let statement =+ prompt('Enter your number')
 
-if (statement % 2 == 0){
-    console.log('you are a number is even')
-}
-else{
-    console.log('you are a number is odd')
-}
-var name = "usman"
-// tada type of programming languages
-// " " '' representing language
+// if (statement % 2 == 0){
+//     console.log('you are a number is even')
+// }
+// else{
+//     console.log('you are a number is odd')
+// }
+// var name = "usman"
+// // tada type of programming languages
+// // " " '' representing language
     
-// strict programming language  c++, Java, typescript
+// // strict programming language  c++, Java, typescript
 
 
 
-//  non strict programming  language - js, python....
+// //  non strict programming  language - js, python....
 
-//  pascal case - first later always capital for each word
+// //  pascal case - first later always capital for each word
 
 
-//  how to ckeak data type in { typeof()}
+// //  how to ckeak data type in { typeof()}
  
-function abc(a , b){
+// function abc(a , b){
     
-    var result = a + b
-    console.log("sum is = ", result)
-}
+//     var result = a + b
+//     console.log("sum is = ", result)
+// }
 
-abc(2,4)
+// abc(2,4)
 
-var firstName = '2323,43'
-var number = 2343
-var flag = false
+// var firstName = '2323,43'
+// var number = 2343
+// var flag = false
 
-console.log('firstName ', typeof(firstName))
-console.log('number ', typeof(number))
-console.log('flag ', typeof(flag))
-var num1 = "121"
-var num2 = 121
-console.log("sum is =>", num1 + num2)
-// concat opratar +
-// + can be done two things> 1- it can be math operation or 2- it can be concat operation
-// How we can deside about + operator, that referring to concat or simple  math plus?
+// console.log('firstName ', typeof(firstName))
+// console.log('number ', typeof(number))
+// console.log('flag ', typeof(flag))
+// var num1 = "121"
+// var num2 = 121
+// console.log("sum is =>", num1 + num2)
+// // concat opratar +
+// // + can be done two things> 1- it can be math operation or 2- it can be concat operation
+// // How we can deside about + operator, that referring to concat or simple  math plus?
 
-var num1 = "5"
+var num1 = 6
 var num2 = 5
-var num3 = 5
+// var num3 = 5
 
-num1 = parseInt(num1)
-console.log(typeof(num1))
+// num1 = parseInt(num1)
+// console.log(typeof(num1))
 
-// write a js programe to convert c temp into f ?
-// (32°F − 32) × 5/9 = 0°C
+// // write a js programe to convert c temp into f ?
+// // (32°F − 32) × 5/9 = 0°C
 
 var tempInC =+ prompt("enter your tampracher")
-var tempInF = (tempInC* 9/5) + 32  
+// var tempInF = (tempInC* 9/5) + 32  
  
 
-console.log("temp In F", tempInF)
+// console.log("temp In F", tempInF)
+
+var num4 = 9
+var num5 = 10
+
+var flag = num1 >= num2 && num5 >= num4 
+
+console.log(flag);
