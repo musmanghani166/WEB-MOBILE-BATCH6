0265a98233d4fd73612b7d73ad89b62b059a5e6f
usman ghani <musmanghani166@gmail.com>
2023-04-01 21:48:47 +0500

    for loop ,while loop, do while loop

--- Javascript/Assignments Projects/practice sir naveed/index.js
+++ Javascript/Assignments Projects/practice sir naveed/index.js
@@ -9,7 +9,7 @@
 // var name = "usman"
 // // tada type of programming languages
 // // " " '' representing language
-    
+
 // // strict programming language  c++, Java, typescript
 
 
@@ -20,9 +20,9 @@
 
 
 // //  how to ckeak data type in { typeof()}
- 
+
 // function abc(a , b){
-    
+
 //     var result = a + b
 //     console.log("sum is = ", result)
 // }
@@ -55,7 +55,7 @@
 
 // var tempInC =+ prompt("enter your tampracher")
 // var tempInF = (tempInC* 9/5) + 32  
- 
+
 
 // console.log("temp In F", tempInF)
 
@@ -78,37 +78,65 @@
 // evenOdd()
 
 // Switch Statement
+// var day = "Mon"
 
-switch (day) {
-    case "Mon":
-        console.log("today is Monday")
-        
-        break;
-    case "Tus":
-        console.log("today is Tuesday")
-        
-        break;
-    case "Wed":
-        console.log("today is Wednesday")
-        
-        break;
-    case "Thr":
-        console.log("today is Thursday")
-        
-        break;
-    case "Fri":
-        console.log("today isFriday")
-        
-        break;
-    case "Sat":
-        console.log("today isSaturday")
-        
-        break;
-    case "Sun":
-        console.log("today is Sunday")
-        
-        break;
+//  switch (day) {
+//     case "Mon":
+//         console.log("today is Monday")
 
-    default:
-        break;
+//         break;
+//     case "Tus":
+//         console.log("today is Tuesday")
+
+//         break;
+//     case "Wed":
+//         console.log("today is Wednesday")
+
+//         break;
+//     case "Thr":
+//         console.log("today is Thursday")
+
+//         break;
+//     case "Fri":
+//         console.log("today isFriday")
+
+//         break;
+//     case "Sat":
+//         console.log("today isSaturday")
+
+//         break;
+//     case "Sun":
+//         console.log("today is Sunday")
+
+//         break;
+
+//     default:
+//         break;
+// }
+// 
+
+
+
+// loop - repeat
+// Type of loop
+// 1-for loop
+// While loop
+// do while loop
+// For each loop
+// map loop filter loop
+// for in loop
+
+
+// While Loop
+var num = 0
+while (num < 50) {
+    console.log(" while loop")
+    num++;
 }
+
+
+// Do while loop
+do {
+    console.log("do while loop")
+    num++;
+} while (num < 50);
