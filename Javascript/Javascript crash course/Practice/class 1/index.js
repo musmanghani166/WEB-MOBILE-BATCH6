1b0ed304508e746b0624e64aad8a6d9523be5044
usman ghani <musmanghani166@gmail.com>
2023-04-05 19:55:49 +0500

    Javascript crash course Class 1

---
+++ Javascript/Javascript crash course/Practice/class 1/index.js
@@ -0,0 +1,21 @@
+// ____________________________________Class #1_________________________________________________
+
+// let name1 = "Usman"
+
+//  alert(name1)
+
+//   Variable Rule ; 
+
+// Legal or Illegal name
+// Use camelCase for long variable names 
+// legal space
+// let firstName = "usman"
+// Illegal space
+// let first Name = "usman"
+// Allowed js special character
+// _ $ &
+// Variable of start cannot be number
+
+
+// _________________________________Class #2______________________________________________________
+
