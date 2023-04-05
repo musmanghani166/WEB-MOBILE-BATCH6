601b33ff76bc2035de0562078c8d8929231a39be
usman ghani <musmanghani166@gmail.com>
2023-04-05 20:41:49 +0500

    js crash course class 2

--- Javascript/Javascript crash course/Practice/class 1/index.js
+++ Javascript/Javascript crash course/Practice/class 1/index.js
@@ -1,15 +1,15 @@
 // ____________________________________Class #1_________________________________________________
 
-// let name1 = "Usman"
+let name1 = "Usman"
 
-//  alert(name1)
+ alert(name1)
 
 //   Variable Rule ; 
 
 // Legal or Illegal name
 // Use camelCase for long variable names 
 // legal space
-// let firstName = "usman"
+let firstName = "usman"
 // Illegal space
 // let first Name = "usman"
 // Allowed js special character
@@ -19,3 +19,24 @@
 
 // _________________________________Class #2______________________________________________________
 
+//  concatenation 
+// let abc = a + b
+let name2 = "Usman"
+// Check inside variable
+let name3 = name2.includes("U")
+console.log(name2)
+// Output ; true
+
+// How to convert a variable from a string to a number
+// 1 ; start in +
+let num1 = "5"
+console.log(+num1)
+// 2 ; start in function
+console.log(parseInt(num1));
+//Use to Random number
+let randomNum = Math.random()
+randomNum = Math.random() * 10
+console.log(randomNum);
+// . To eliminate the upper one
+randomNum = Math.round(randomNum)
+console.log(randomNum);
