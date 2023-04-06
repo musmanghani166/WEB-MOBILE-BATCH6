33a9ca5ca5ec68653322b4a1ae7bc970bc263c0a
usman ghani <musmanghani166@gmail.com>
2023-04-06 19:21:31 +0500

    Js crash course assignment 1

--- Javascript/Javascript crash course/Assignments/assignment 1/index.js
+++ Javascript/Javascript crash course/Assignments/assignment 1/index.js
@@ -1,10 +1,111 @@
-let age =+ prompt("Please enter your Age")
-if (age === 10) {
-    console.log("Your are a Baby");
-} else {
-    console.log("10 years above not allowed");
-}
-// let table = 8 "X" index 
-for (let index = 1; index <= 10; index++) {
-    console.log( "8 X " + index + ' = ' + 8 *index);
-}
+// //  Q:No:1  JavaScript program that accepts two numbers in two prompts and  displays the larger one in the console
+ 
+//  let num1 =+ prompt("Enter your first number")
+//  let num2 =+ prompt("Enter your second number")
+//   if (num1 > num2) {
+//     console.log("your first number is large", num1);
+//   } else {
+//     console.log("your second number is large",num2);
+//   }
+
+// // ------------------------------------------------------------------------------------------------------------- //
+
+// // Q:No:2 JavaScript conditional statement to find the sign of a number. Display an alert box with the specified sign
+
+// let num1 =+ prompt("Enter your any number")
+//  if (num1 >= 0) {
+//     console.log("The sign is +");
+//  } else {
+//     console.log("The sign is -");
+//  }
+
+//  // ------------------------------------------------------------------------------------------------------------- //
+
+// // Q;No:3 a JavaScript program that accepts five numbers in five prompts and displays the larger one in the console
+
+// let num1 =+ prompt("Enter your 1st number")
+// let num2 =+ prompt("Enter your 2nd number")
+// let num3 =+ prompt("Enter your 3rd number")
+// let num4 =+ prompt("Enter your 4th number")
+// let num5 =+ prompt("Enter your 5th number")
+ 
+// if (num2 > num1) {
+//  console.log("your 2nd number is large");   
+// }else if (num3 > num1) {
+//     console.log("your 3rd number is large") 
+// }
+// else if (num4 > num1) {
+//     console.log("your 4th number is large") 
+// }
+// else if (num5 > num1) {
+//     console.log("your 5th number is large") 
+// }
+// else{
+//     console.log("your 1st number is large");
+// }
+
+//  // ------------------------------------------------------------------------------------------------------------- //
+
+// // Q:No:4 a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen
+
+// for (let i = 0; i <= 15; i++) {
+//  if (i % 2 == 0) {
+//     console.log(i + " is even");
+//  } else {
+//     console.log(i + " is odd");
+//  }
+    
+// }
+  
+// // ------------------------------------------------------------------------------------------------------------- //
+
+// // Q:No:5 JavaScript program which computes the average marks of the following students Then, this average is used to determine the corresponding grade
+ 
+// let marks =+ prompt("Enter your marks");
+// if (marks >= 90) {
+//     console.log("Your marks is A Grade");
+// }else if (marks >= 80) {
+//     console.log("Your marks is B Grade");
+// }
+// else if (marks >= 70) {
+//     console.log("Your marks is C Grade");
+// }
+// else if (marks >= 60) {
+//     console.log("Your marks is D Grade");
+// }
+// else{
+//     console.log("Your marks is F Grade");
+// }
+
+//  // ------------------------------------------------------------------------------------------------------------- //
+
+// // Q:No:6 JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
+ 
+// for (let i = 1; i < 100;  i++) {
+//  if (i % 3 === 0 && 1 % 5 === 0) {
+//     console.log("FizzBuzz");
+//  }else if (i % 3 === 0) {
+//     console.log("Fizz");
+//  }else if(i % 5 === 0){
+//     console.log("Buzz");
+//  }else{
+//     console.log(1);
+//  }
+    
+// }
+
+// // ------------------------------------------------------------------------------------------------------------- //
+
+// Q:No:7 JavaScript program to construct the following pattern, using a nested for loop
+
+ 
+
+// for (let index = 1; index <= 5; index++) {
+//     let star = ""
+//     for (let i = 1; i <= index ; i++) {
+//         star += "* ";
+//     }
+//     console.log(star);
+// }
+ 
+// ------------------------------------------------------------------------------------------------------------- //
