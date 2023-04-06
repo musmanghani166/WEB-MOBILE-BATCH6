e145a303bab442adc719b7b03cd5f184fea989c4
usman ghani <musmanghani166@gmail.com>
2023-04-06 14:13:34 +0500

    js crash course assignment 1

---
+++ Javascript/Javascript crash course/Assignments/assignment 1/index.js
@@ -0,0 +1,10 @@
+let age =+ prompt("Please enter your Age")
+if (age === 10) {
+    console.log("Your are a Baby");
+} else {
+    console.log("10 years above not allowed");
+}
+// let table = 8 "X" index 
+for (let index = 1; index <= 10; index++) {
+    console.log( "8 X " + index + ' = ' + 8 *index);
+}
