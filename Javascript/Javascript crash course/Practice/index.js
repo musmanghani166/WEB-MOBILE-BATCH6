af91f4347fdabe274f62e9d8ee91334c27da670f
usman ghani <musmanghani166@gmail.com>
2023-04-08 01:03:27 +0500

    js crash course

--- Javascript/Javascript crash course/Practice/index.js
+++ Javascript/Javascript crash course/Practice/index.js
@@ -1,56 +1,82 @@
-// ____________________________________Class #1_________________________________________________
+// // ____________________________________Class #1_________________________________________________
 
-let name1 = "Usman"
+// let name1 = "Usman"
 
- alert(name1)
+//  alert(name1)
 
-//   Variable Rule ; 
+// //   Variable Rule ; 
 
-// Legal or Illegal name
-// Use camelCase for long variable names 
-// legal space
-let firstName = "usman"
-// Illegal space
-// let first Name = "usman"
-// Allowed js special character
-// _ $ &
-// Variable of start cannot be number
+// // Legal or Illegal name
+// // Use camelCase for long variable names 
+// // legal space
+// let firstName = "usman"
+// // Illegal space
+// // let first Name = "usman"
+// // Allowed js special character
+// // _ $ &
+// // Variable of start cannot be number
 
 
-// _________________________________Class #2______________________________________________________
+// // _________________________________Class #2______________________________________________________
 
-//  concatenation 
-// let abc = a + b
-let name2 = "Usman"
-// Check inside variable
-let name3 = name2.includes("U")
-console.log(name2)
-// Output ; true
+// //  concatenation 
+// // let abc = a + b
+// let name2 = "Usman"
+// // Check inside variable
+// let name3 = name2.includes("U")
+// console.log(name2)
+// // Output ; true
 
-// How to convert a variable from a string to a number
-// 1 ; start in +
-let num1 = "5"
-console.log(+num1)
-// 2 ; start in function
-console.log(parseInt(num1));
-//Use to Random number
-let randomNum = Math.random()
-randomNum = Math.random() * 10
-console.log(randomNum);
-// . To eliminate the upper one
-randomNum = Math.round(randomNum)
-console.log(randomNum);
+// // How to convert a variable from a string to a number
+// // 1 ; start in +
+// let num1 = "5"
+// console.log(+num1)
+// // 2 ; start in function
+// console.log(parseInt(num1));
+// //Use to Random number
+// let randomNum = Math.random()
+// randomNum = Math.random() * 10
+// console.log(randomNum);
+// // . To eliminate the upper one
+// randomNum = Math.round(randomNum)
+// console.log(randomNum);
 
-// _________________________________Class #3______________________________________________________
+// // _________________________________Class #3______________________________________________________
 
-// coparison oprater 
-//  == , === 
-// == : value cheak
-// === : value and data type cheak
+// // coparison oprater 
+// //  == , === 
+// // == : value cheak
+// // === : value and data type cheak
 
-// for loop
-for (let index = 0; index < 10; index++) {
-     console.log("hello")
+// // for loop
+// for (let index = 0; index < 10; index++) {
+//      console.log("hello")
+// }
+
+// // _________________________________Class #4______________________________________________________
+
+// // Function
+// function name1(studentName){
+//     console.log(studentName)
+// }
+// name1("Usman")
+// // built-in-function
+// // alert()
+// // parseInt()
+// // etc.
+//   function add(num1, num2) {
+//     return num1 + num2
+
+//   }
+//   let result = add(5,7)
+//   console.log(result);
+// Recursion
+function writeData(data) {
+    document.write(data)
+    if (time70) {
+        write(data, time-1)
+    }
+
 }
-
-// _________________________________Class #4______________________________________________________
+console.log("hello ",5);
+ 
