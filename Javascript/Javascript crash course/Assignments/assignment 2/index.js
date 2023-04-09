a5aa4b20768862414998f5ef78fa0042c58febc1
usman ghani <musmanghani166@gmail.com>
2023-04-09 12:46:39 +0500

    crash course js Q:No;3

--- Javascript/Javascript crash course/Assignments/assignment 2/index.js
+++ Javascript/Javascript crash course/Assignments/assignment 2/index.js
@@ -1,6 +1,6 @@
-// // Q:No:1 a function that creates a closure and returns a function that can add
-// // a specific number to any number passed to it. For example, if the closure is
-// // created with 5, the returned function should add 5 to any number passed to it
+// // Q:No:1 a function that creates a closure and returns a function that can add a specific number to any number passed to it. 
+// //  For example, if the closure is  created with 5, the returned function should add 5 to any number passed to it
+ 
 
 // let num1 =+ prompt("Enter your number")
 // function addNum(num) {
@@ -13,9 +13,9 @@
 
 // // ------------------------------------------------------------------------------------------------------- // 
 
-// // Q:No:2 Write a recursive function that searches an array for a specific value. The
-// // function should return true if the value is found, and false if it is not. You
-// // can assume that the array is not nested
+// // Q:No:2 Write a recursive function that searches an array for a specific value. The function should return true if the value
+// //is found, and false if it is not. You can assume that the array is not nested
+ 
 // let array1 = [1, 2, 3, 4, 5];
 // let array2 = [6, 7, 8]
 // function functionArray(arr, val) {
@@ -31,3 +31,6 @@
 // console.log(functionArray(array2, 8)); 
 
 // // ------------------------------------------------------------------------------------------------------- // 
+
+// Q:No:3 Write a function that adds a new paragraph element to the bottom of an HTML document. The function should take a string argument that will be
+// used as the text content of the new paragraph element.
